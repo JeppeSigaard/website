3e34944280a16ed4322c1a6f56029584fc5fec6e
Jeppe Sigaard <jelis@jfm.dk>
2023-09-08 10:25:48 +0200

    basic setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  site: "https://jeppesigaard.github.io",
+  base: "/website",
+  integrations: [react()],
+  output: "server",
+  adapter: nodejs(),
 });
